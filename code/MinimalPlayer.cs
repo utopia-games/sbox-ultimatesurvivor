ce5e7f1e4c852cd3eec808e782992ff016b3f41c
Guilhem PECH <guilhempech@gmail.com>
2021-05-27 20:00:30 +0200

    :tada: Initial commit

---
+++ code/MinimalPlayer.cs
@@ -0,0 +1,64 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace MinimalExample
+{
+	partial class MinimalPlayer : Player
+	{
+		public override void Respawn()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+
+			//
+			// Use WalkController for movement (you can make your own PlayerController for 100% control)
+			//
+			Controller = new WalkController();
+
+			//
+			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
+			//
+			Animator = new StandardPlayerAnimator();
+
+			//
+			// Use ThirdPersonCamera (you can make your own Camera for 100% control)
+			//
+			//Camera = new ThirdPersonCamera();
+			Camera = new FirstPersonCamera();
+			
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true; 
+			
+			base.Respawn();
+		}
+		 
+		[Event("hotloaded")]
+		public void OnHotReload()
+		{
+			
+		}
+		
+		/// <summary>
+		/// Called every tick, clientside and serverside.
+		/// </summary>
+		public override void Simulate( Client cl )
+		{
+			base.Simulate( cl );
+
+			//
+			// If we're running serverside and Attack1 was just pressed, spawn a ragdoll
+			//
+			if ( IsServer && Input.Pressed( InputButton.Attack1 ) )
+			{
+				var ragdoll = new ModelEntity();
+				ragdoll.SetModel( "models/citizen/citizen.vmdl" );  
+				ragdoll.Position = EyePos + EyeRot.Forward * 40;
+				ragdoll.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+				ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+				ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 1000;
+			}
+		}
+	}
+}
